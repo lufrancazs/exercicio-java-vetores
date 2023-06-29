17a42331eec815a67e07874db0260089f1253204
Lucas Martins <lucasfranca_zs@hotmail.com>
2023-06-28 21:15:30 -0300

    Exercicio de Numeros Pares - Vetores

---
+++ exercicio_vetores/problema_numeros_pares.java
@@ -0,0 +1,41 @@
+package exercicio_vetores;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class problema_numeros_pares {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Quantos números você vai digitar: ");
+		int n = sc.nextInt();
+
+		int[] vect = new int[n];
+
+		for (int i = 0; i < vect.length; i++) {
+			System.out.print("Digite um número: ");
+			vect[i] = sc.nextInt();
+		}
+
+		int sum = 0;
+
+		System.out.println("NUMEROS PARES:");
+
+		for (int i = 0; i < vect.length; i++) {
+
+			if (vect[i] % 2 == 0) {
+				System.out.print(" " + vect[i]);
+				sum += 1;
+			}
+		}
+		System.out.println();
+		System.out.println("QUANTIDADE DE PARES = " + sum);
+
+		sc.close();
+
+	}
+
+}
