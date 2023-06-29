8fb9938dd9cd7f128510a02ef0501173777c8b19
Lucas Martins <lucasfranca_zs@hotmail.com>
2023-06-28 21:29:34 -0300

    Exercicio negativos

---
+++ exercicio_vetores/problema_negativos.java
@@ -0,0 +1,36 @@
+package exercicio_vetores;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class problema_negativos {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("Quantos números você vai digitar? ");
+		int n = sc.nextInt();
+		
+		int[] vect = new int[n];
+		
+		for(int i = 0; i < vect.length; i++) {
+			System.out.print("Digite um número: ");
+			vect[i] = sc.nextInt();
+		}
+		
+		System.out.println("Números Negativos:");
+		
+		for(int i = 0; i < vect.length; i++) {
+			
+			if(vect[i] < 0) {
+				System.out.println(vect[i]);
+			}
+		}
+
+		sc.close();
+	}
+
+}
